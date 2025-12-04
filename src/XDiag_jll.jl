bd8664587f05934388d294cb0a3c2cf23fbe91e3
jlbuild <juliabuildbot@gmail.com>
2025-12-04 04:50:56 +0000

    XDiag_jll build 0.4.1+0

--- src/XDiag_jll.jl
+++ src/XDiag_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("XDiag")
-JLLWrappers.@generate_main_file("XDiag", UUID("05e47fe5-458f-5313-a276-ec18ac0ef94c"))
+JLLWrappers.@generate_main_file("XDiag", Base.UUID("05e47fe5-458f-5313-a276-ec18ac0ef94c"))
 end  # module XDiag_jll
